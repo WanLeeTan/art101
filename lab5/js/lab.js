8df5d5f6996c40ccf6f8c04935d4a594d2b48e92
WanLeeTan <42859246+WanLeeTan@users.noreply.github.com>
2022-04-20 20:49:53 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+  * Author: Wan Lee Tan and Gavin Concepcion
+  * Created: 4/20/2022
+**/
+
+// define variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+ownIt = true;
+
+//calculate
+age = 2019 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years" + "<br>");
+document.writeln("Own It: " + ownIt + "<br>");
