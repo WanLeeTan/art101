42169a0e8ba0f09e986938fce3dfd628d883cf6d
WanLeeTan <42859246+WanLeeTan@users.noreply.github.com>
2022-05-11 21:56:12 -0700

    Lab 11

---
+++ lab11/js/lab.js
@@ -0,0 +1,24 @@
+/**
+  * Author: Wan Lee Tan and Gavin Concepcion
+  * Created: 5/09/2022
+**/
+
+//buttons to change the colors
+$("button.purple").click(function(){
+  		// make it purple
+      $("#challenges").removeClass("pink");
+      $("#challenges").toggleClass("purple");
+      //make it pink
+      $("#results").removeClass("purple");
+      $("#results").toggleClass("pink");
+});
+
+//buttons to change the colors
+$("button.pink").click(function(){
+  		// make it pink
+      $("#challenges").removeClass("purple");
+      $("#challenges").toggleClass("pink");
+      // make it purple
+      $("#results").removeClass("pink");
+      $("#results").toggleClass("purple");
+});
